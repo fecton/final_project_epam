3181de3321aaee27b67c69347dc19c6e664d1d59
fecton <bopongamer@gmail.com>
2022-11-27 13:28:34 +0000

    updated shell scripts

---
+++ 2. Ansible - Setup the infrastructure/JenkinsStartUp.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Installing Jenkins + SSH
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install jenkins openjdk-8-jdk openjdk-11-jdk -y
+sudo apt install -fy
+
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+# Installing Docker Engine
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo systemctl enable docker
+sudo systemctl start docker
+
+
+
+
---
+++ 2. Ansible - Setup the infrastructure/ProductionStartUp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# Installing Docker Engine
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo systemctl enable docker
+sudo systemctl start docker
+
+
+
+
